f42d0ec7ed6e080272b6453ba9699789fc5db278
Tuls9999 <96272507+Tuls9999@users.noreply.github.com>
2023-01-20 09:24:07 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Tulsi Gandhi Student ID: 100673169 Date: 01/20/23
+*
+*  Online (Cyclic) URL: https://app.cyclic.sh/#/deploy/Tuls9999/Hello-World-Assign1
+*
+********************************************************************************/ 
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
